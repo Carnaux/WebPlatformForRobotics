2c17b11d1eb3bde6a00928a6892092475a97ba09
Carnaux <danielfeenandes@gmail.com>
2018-11-17 21:15:26 -0300

    changes

--- Interface/j1.js
+++
@@ -1,8 +0,0 @@
-function hide(){
-	document.getElementById('container-all').style.visibility = "hidden";
-	document.getElementById('motorcontrol').style.visibility = "visible";
-}
-function reveal() {
-	document.getElementById('motorcontrol').style.visibility = "hidden";
-	document.getElementById('container-all').style.visibility = "visible";
-}    
